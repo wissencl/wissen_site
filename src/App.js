1d6a2f41e247a1a0aa1a369980f4a70c00263ce5
Jose Candia <josecandiap@gmail.com>
2022-02-22 18:33:39 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { ModalProyect } from "./components/ModalProyect";
+import { ProyectCard } from "./components/ProyectCard";
+
+function App() {
+  const valores = [{
+    titulo: 'Prueba',
+    descripcion: 'Culpa labore velit pariatur duis veniam exercitation qui eiusmod veniam magna voluptate non excepteur sit. Do culpa esse fugiat fugiat. Elit ut proident officia mollit sunt eu voluptate. Eu proident officia excepteur tempor aute nulla Lorem eiusmod est in ea commodo in.',
+    tecnologias: [
+      'react_logo.png',
+      'angular_logo.png'
+    ]
+  },
+  {
+    titulo: 'Prueba 1',
+    descripcion: 'Culpa labore velit pariatur duis veniam exercitation qui eiusmod veniam magna voluptate non excepteur sit. Do culpa esse fugiat fugiat. Elit ut proident officia mollit sunt eu voluptate. Eu proident officia excepteur tempor aute nulla Lorem eiusmod est in ea commodo in.',
+    tecnologias: [
+      'react_logo.png',
+      'tailwind_logo.png'
+    ]
+  },
+  {
+    titulo: 'Prueba 2',
+    descripcion: 'Culpa labore velit pariatur duis veniam exercitation qui eiusmod veniam magna voluptate non excepteur sit. Do culpa esse fugiat fugiat. Elit ut proident officia mollit sunt eu voluptate. Eu proident officia excepteur tempor aute nulla Lorem eiusmod est in ea commodo in.',
+    tecnologias: [
+      'dart_logo.png',
+      'tailwind_logo.png'
+    ]
+  },
+  {
+    titulo: 'Prueba 3',
+    descripcion: 'Culpa labore velit pariatur duis veniam exercitation qui eiusmod veniam magna voluptate non excepteur sit. Do culpa esse fugiat fugiat. Elit ut proident officia mollit sunt eu voluptate. Eu proident officia excepteur tempor aute nulla Lorem eiusmod est in ea commodo in.',
+    tecnologias: [
+      'dart_logo.png',
+      'tailwind_logo.png'
+    ]
+  },
+  {
+    titulo: 'Prueba 4',
+    descripcion: 'Culpa labore velit pariatur duis veniam exercitation qui eiusmod veniam magna voluptate non excepteur sit. Do culpa esse fugiat fugiat. Elit ut proident officia mollit sunt eu voluptate. Eu proident officia excepteur tempor aute nulla Lorem eiusmod est in ea commodo in.',
+    tecnologias: [
+      'dart_logo.png',
+      'tailwind_logo.png'
+    ]
+  },
+  {
+    titulo: 'Prueba 5',
+    descripcion: 'Culpa labore velit pariatur duis veniam exercitation qui eiusmod veniam magna voluptate non excepteur sit. Do culpa esse fugiat fugiat. Elit ut proident officia mollit sunt eu voluptate. Eu proident officia excepteur tempor aute nulla Lorem eiusmod est in ea commodo in.',
+    tecnologias: [
+      'react_logo.png',
+      'tailwind_logo.png'
+    ]
+  },
+  {
+    titulo: 'Prueba 6',
+    descripcion: 'Culpa labore velit pariatur duis veniam exercitation qui eiusmod veniam magna voluptate non excepteur sit. Do culpa esse fugiat fugiat. Elit ut proident officia mollit sunt eu voluptate. Eu proident officia excepteur tempor aute nulla Lorem eiusmod est in ea commodo in.',
+    tecnologias: [
+      'react_logo.png',
+      'angular_logo.png'
+    ]
+  }]
+
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 justify-evenly">
+        {
+          valores.map((value, i) => (
+            <>
+              <ProyectCard
+                key = { i }
+                titulo = { value.titulo }
+                descripcion = { value.descripcion }
+                tecnologias = { value.tecnologias }
+              />
+            </>
+          ))
+        }
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ProyectCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ModalProyect } from './ModalProyect';
+
+export const ProyectCard = ({ titulo, descripcion, tecnologias }) => {
+  return (
+    <>
+      <div className="max-w-sm lg:w-72 sm:w-72 m-10 lg:max-w-full cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" data-modal-toggle="defaultModal">
+          <div className="border-r border-b border-l border-t border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-lg p-4 flex flex-col justify-between leading-normal">
+          <div className="mb-8">
+            <div className="text-gray-900 font-bold text-xl mb-4">{ titulo }</div>
+            <p className="text-gray-700 text-justify">{ descripcion }</p>
+          </div>
+          <div className="flex items-center">
+              {
+                tecnologias.map( (value, i) => (
+                  <img key = { i } className="w-10 h-10 rounded-full mr-4 object-contain" src={ `/assets/images/${ value }`} />
+                ))
+              }
+              <div className="text-sm">
+              <p className="text-gray-900 leading-none">Jonathan Reinink</p>
+              <p className="text-gray-600">Aug 18</p>
+              </div>
+          </div>
+          </div>
+      </div>
+      <ModalProyect />
+    </>
+  );
+};
